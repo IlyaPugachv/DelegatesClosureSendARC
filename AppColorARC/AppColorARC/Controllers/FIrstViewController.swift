295e2a1b6e0e82668eb9153fadecd3669133af8d
Ilya Pugachev <ilyapugachv@gmail.com>
2023-09-10 14:19:53 +0300

    Color App final

--- AppColorARC/AppColorARC/Controllers/FIrstViewController.swift
+++ AppColorARC/AppColorARC/Controllers/FIrstViewController.swift
@@ -6,27 +6,33 @@ protocol DataUpdateProtocol {
 
 class FIrstViewController: UIViewController {
     
-    
     @IBOutlet weak var ColorView: UIView! { didSet {
         ColorView.backgroundColor = color
-        if let transparency = transparency {
-            ColorView.alpha = transparency
-        }
-    }
-    }
+        if let transparency = transparency { ColorView.alpha = transparency } } }
+    
     @IBOutlet weak var changedBackground: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
     
     var color: UIColor?
     var transparency: CGFloat?
     var handler: (() -> ())?
+    var delegate: DataUpdateProtocol?
     
     @IBAction func ChangeBGAction() {
-       
         let stor = UIStoryboard(name: "Main", bundle: nil)
         guard let secondViewController = stor.instantiateViewController(withIdentifier: "SecondViewController") as? SecondViewController else { return }
         
         secondViewController.delegate = self
         navigationController?.pushViewController(secondViewController, animated: true)
+    }
+    
+    private func setupUI() {
+        changedBackground.backgroundColor = .white
+        changedBackground.layer.cornerRadius = 12
     }
 }
 
--- AppColorARC/AppColorARC/Controllers/SecondViewController.swift
+++ AppColorARC/AppColorARC/Controllers/SecondViewController.swift
@@ -4,8 +4,8 @@ import Foundation
 class SecondViewController: UIViewController {
     
     // MARK: - IBOutlet RED
-    @IBOutlet private weak var hsRed: UISlider!
-    @IBOutlet private weak var tfRed: UITextField!
+    @IBOutlet  weak var hsRed: UISlider!
+    @IBOutlet  weak var tfRed: UITextField!
     
     // MARK: - IBOutlet GREEN
     @IBOutlet private weak var hsGreen: UISlider!
@@ -17,7 +17,7 @@ class SecondViewController: UIViewController {
     
     // MARK: - IBOutlet COLOR TEXT FIELD
     @IBOutlet private weak var tfHerColor: UITextField!
-
+    
     // MARK: - IBOutlet OPARACITY
     @IBOutlet private weak var hsOpacity: UISlider!
     @IBOutlet private weak var tfOpacity: UITextField!
@@ -32,31 +32,20 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-       
     }
     
-       
-        var delegate: DataUpdateProtocol?
+    var delegate: DataUpdateProtocol?
+    
+    @IBAction func hsRedAction(_ sender: UISlider) {
+        let redValue = Int(round(hsRed.value))
+        tfRed.text = String(redValue)
         
-        
-        @IBAction func hsRedAction(_ sender: UISlider) {
-
-            let redValue = Int(round(hsRed.value))
-            tfRed.text = String(redValue)
-            
-            let color = rgbToHex(red: redValue, green: Int(hsGreen.value), blue: Int(hsBlue.value))
-            tfHerColor.text = color
-            colorVIew()
-            
-        }
-        
-        @IBAction func tfRedAction(_ sender: UITextField) {
-            
-            
-        }
-        
+        let color = rgbToHex(red: redValue, green: Int(hsGreen.value), blue: Int(hsBlue.value))
+        tfHerColor.text = color
+        colorVIew()
+    }
+    
     @IBAction func hsGreenAction(_ sender: UISlider) {
-        
         let greenValue = Int(round(hsGreen.value))
         tfGreen.text = String(greenValue)
         
@@ -64,13 +53,8 @@ class SecondViewController: UIViewController {
         tfHerColor.text = color
         colorVIew()
     }
-        
-        @IBAction func tfGreenAction(_ sender: UITextField) {
-            
-        }
-        
+    
     @IBAction func hsBlueAction(_ sender: UISlider) {
-        
         let blueValue = Int(round(hsBlue.value))
         tfBlue.text = String(blueValue)
         
@@ -78,28 +62,13 @@ class SecondViewController: UIViewController {
         tfHerColor.text = color
         colorVIew()
     }
-        
-        
-        @IBAction func tfBlueAction(_ sender: UITextField) {
-            
-        }
-        
-        
-        @IBAction func colorTFAction(_ sender: UITextField) {
-            
-        }
-        
-        
-        @IBAction func opacityHSAction(_ sender: UISlider) {
-            let value = Int(sender.value)
-            tfOpacity.text = "\(value)%"
-            opacityView()
-        }
-        
-        @IBAction func tfActionOpacity(_ sender: UITextField) {
-            
-        }
-        
+    
+    @IBAction func opacityHSAction(_ sender: UISlider) {
+        let value = Int(sender.value)
+        tfOpacity.text = "\(value)%"
+        opacityView()
+    }
+    
     @IBAction func delegateButtonAction(_ sender: UIButton) {
         let stor = UIStoryboard(name: "Main", bundle: nil)
         guard let FIrstViewController = stor.instantiateViewController(withIdentifier: "FIrstViewController") as? FIrstViewController else { return }
@@ -107,8 +76,8 @@ class SecondViewController: UIViewController {
         FIrstViewController.transparency = colorView.alpha
         
         self.navigationController?.pushViewController(FIrstViewController, animated: true)
-        }
-        
+    }
+    
     @IBAction func closureButtonAction(_ sender: UIButton) {
         let stor = UIStoryboard(name: "Main", bundle: nil)
         guard let FIrstViewController = stor.instantiateViewController(withIdentifier: "FIrstViewController") as? FIrstViewController else { return }
@@ -118,51 +87,45 @@ class SecondViewController: UIViewController {
         FIrstViewController.handler = { [unowned self]  in
             self.colorView.backgroundColor = FIrstViewController.ColorView.backgroundColor
             self.colorView.alpha = FIrstViewController.changedBackground.alpha
-            
-            
         }
-        
     }
     
+    private func colorVIew() {
+        self.colorView.backgroundColor = UIColor(red: CGFloat(hsRed.value)/255, green: CGFloat(hsGreen.value)/255, blue: CGFloat(hsBlue.value)/255, alpha: 1)
+    }
     
-    
-    
-    
-        func colorVIew() {
-            self.colorView.backgroundColor = UIColor(red: CGFloat(hsRed.value)/255, green: CGFloat(hsGreen.value)/255, blue: CGFloat(hsBlue.value)/255, alpha: 1)
-         
-            
-        }
+    private func setupUI() {
+        hsRed.minimumValue = 0
+        hsRed.maximumValue = 250
         
-        func setupUI() {
-            hsRed.minimumValue = 0
-            hsRed.maximumValue = 250
-            
-            hsGreen.minimumValue = 0
-            hsGreen.maximumValue = 250
-            
-            hsBlue.minimumValue = 0
-            hsBlue.maximumValue = 250
-            
-            
-            hsOpacity.minimumValue = 0
-            hsOpacity.maximumValue = 100
-            
-            colorView.layer.cornerRadius = 12
-            
-            
-        }
-        func opacityView() {
-            let sliderValue = hsOpacity.value
-            let alpha: CGFloat = CGFloat(sliderValue) / CGFloat(100)
-            colorView.alpha = alpha
-        }
+        hsGreen.minimumValue = 0
+        hsGreen.maximumValue = 250
         
+        hsBlue.minimumValue = 0
+        hsBlue.maximumValue = 250
+        
+        
+        hsOpacity.minimumValue = 0
+        hsOpacity.maximumValue = 100
+        
+        colorView.layer.cornerRadius = 12
+        
+        hsRed.tintColor = UIColor.red
+        hsGreen.tintColor = UIColor.green
+        hsBlue.tintColor = UIColor.blue
+    }
+    
+    private func opacityView() {
+        let sliderValue = hsOpacity.value
+        let alpha: CGFloat = CGFloat(sliderValue) / CGFloat(100)
+        colorView.alpha = alpha
+    }
+    
     private func rgbToHex(red: Int, green: Int, blue: Int) -> String {
         let redHex = String(format: "%02X", red)
         let greenHex = String(format: "%02X", green)
         let blueHex = String(format: "%02X", blue)
-
+        
         return "#" + redHex + greenHex + blueHex
     }
     
@@ -170,8 +133,7 @@ class SecondViewController: UIViewController {
         let value = Int(sender.value)
         textField.text = "\(value)"
     }
-       
-    }
+}
 
 
 
